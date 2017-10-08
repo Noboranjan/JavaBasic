fa7a94151c01ebf2878a2f7d775b96ac2cfa365c
Noboranjan <niloydey@ymail.com>
2017-10-08 13:25:49 +0600

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,42 @@
+public class Car{
+	private int milage;
+	private double fuel;
+	private int fuelConsumptionRate;
+	
+	public int getMilage()
+	{
+		return this.milage;
+	}
+	public double getFuel()
+	{
+		return this.fuel;
+	}
+	public double getFuelConsumptionRate()
+	{
+		return this.fuelConsumptionRate;
+	}
+	
+	public  void setMilage(int milage)
+	{
+		this.milage=milage;
+	}
+	public void setFuel(double fuel)
+	{
+		this.fuel=fuel;
+	}
+	public void setFuelConsumptionRate(int fuelConsumptionRate)
+	{
+		this.fuelConsumptionRate = fuelConsumptionRate;
+	}
+	public void drive(int milage)
+	{
+		this.milage+=milage;
+		int burnFuel;
+		burnFuel = fuelConsumptionRate*milage;
+		fuel-=burnFuel;
+		System.out.println("Drive milage "+milage);
+		System.out.println("drive Fuel consumtion rate "+burnFuel);
+		System.out.println("drive fuel preserved "+fuel);
+		
+	}
+}
