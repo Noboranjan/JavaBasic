3c9dd8859c80f648548c94ad6dacd49564025775
Noboranjan <niloydey@ymail.com>
2017-11-12 13:20:33 +0600

    Add files via upload

--- MyGUI.java
+++ MyGUI.java
@@ -1,9 +1,12 @@
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+
+
 public class MyGUI{
 	public static void main(String[] args){
-		JFrame f= new JFrame();
+		JFrame f= new JFrame("My Contact");
 		
 		f.setBounds(200,100,500,500);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -12,12 +15,39 @@ public class MyGUI{
 		 System.out.println("My Gui running......");
 		 //to add a textfield component
 		 JTextField tf1=new JTextField();
-		 tf1.setBounds(100,15,100,30);
+		 tf1.setBounds(150,15,100,30);
 		 f.add(tf1);
 		 
-		 JButton button= new JButton("button");
-		 button.setBounds(150,50,100,25);
+		 JButton button= new JButton("Delete");
+		 button.setBounds(50,200,100,25);
 		 f.add(button);
+		 
+		 JButton button1= new JButton("Save");
+		 button1.setBounds(180,200,100,25);
+		 f.add(button1);
+		 
+		 JLabel jlb=new JLabel("Name");
+		 jlb.setBounds(50,15,80,30);
+		 f.add(jlb);
+		 
+		 JTextField tf2=new JTextField();
+		 tf2.setBounds(150,65,100,30);
+		 f.add(tf2);
+		 
+		 JLabel jlb1=new JLabel("Phone");
+		 jlb1.setBounds(50,65,80,30);
+		 f.add(jlb1);
+		 
+		 JTextField tf3=new JTextField();
+		 tf3.setBounds(150,115,100,30);
+		 f.add(tf3);
+		 
+		 JLabel jlb2=new JLabel("E-mail");
+		 jlb2.setBounds(50,115,80,30);
+		 f.add(jlb2);
+		 
+		 
+		 
 		 
 		 f.setVisible(true);
 		 
